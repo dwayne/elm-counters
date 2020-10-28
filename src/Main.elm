8fa245f698af27e970291f035a18e4f2e1fd1d42
Dwayne Crooks <me@dwaynecrooks.com>
2020-10-28 15:09:02 -0400

    Add different styles of counters

--- src/Main.elm
+++ src/Main.elm
@@ -20,12 +20,12 @@ main =
 type alias Model = List Counter
 
 
-type alias Counter = Int
-
-
 init : () -> (Model, Cmd Msg)
 init _ =
-    ( [0, 5, 10]
+    ( [ counterInt 0
+      , counterNonNegative 5
+      , counterStep 10 100
+      ]
     , Cmd.none
     )
 
@@ -43,22 +43,22 @@ update : Msg -> Model -> (Model, Cmd Msg)
 update msg model =
     case msg of
         Decrement i ->
-            ( listUpdateAt i (\counter -> counter - 1) model
+            ( listUpdateAt i counterDecrement model
             , Cmd.none
             )
 
         Increment i ->
-            ( listUpdateAt i (\counter -> counter + 1) model
+            ( listUpdateAt i counterIncrement model
             , Cmd.none
             )
 
         Reset i ->
-            ( listUpdateAt i (always 0) model
+            ( listUpdateAt i counterReset model
             , Cmd.none
             )
 
         Add n ->
-            ( n :: model
+            ( counterInt n :: model
             , Cmd.none
             )
 
@@ -78,19 +78,105 @@ view model =
     div []
         [ button [ onClick (Add 0) ] [ text "Add" ]
         , button [ onClick AddRandom ] [ text "Add Random" ]
-        , div [] (List.indexedMap viewCounter model)
+        , div [] <|
+            List.indexedMap
+                (\i counter -> counterView (Decrement i) (Increment i) (Reset i) (Remove i) counter)
+                model
         ]
 
 
-viewCounter : Int -> Counter -> Html Msg
-viewCounter i counter =
-    div []
-        [ button [ onClick (Decrement i) ] [ text "-" ]
-        , text (String.fromInt counter)
-        , button [ onClick (Increment i) ] [ text "+" ]
-        , button [ onClick (Reset i) ] [ text "Reset" ]
-        , button [ onClick (Remove i) ] [ text "Remove" ]
-        ]
+-- COUNTER
+
+
+type Counter
+    = Integer Int
+    | NonNegative Int
+    | Step Int Int
+
+
+counterInt : Int -> Counter
+counterInt n =
+    Integer n
+
+
+counterNonNegative : Int -> Counter
+counterNonNegative n =
+    NonNegative (max 0 n)
+
+
+counterStep : Int -> Int -> Counter
+counterStep by n =
+    Step (max 2 by) n
+
+
+counterDecrement : Counter -> Counter
+counterDecrement counter =
+    case counter of
+        Integer n ->
+            Integer (n - 1)
+
+        NonNegative n ->
+            NonNegative (max 0 (n - 1))
+
+        Step by n ->
+            Step by (n - by)
+
+
+counterIncrement : Counter -> Counter
+counterIncrement counter =
+    case counter of
+        Integer n ->
+            Integer (n + 1)
+
+        NonNegative n ->
+            NonNegative (n + 1)
+
+        Step by n ->
+            Step by (n + by)
+
+
+counterReset : Counter -> Counter
+counterReset counter =
+    case counter of
+        Integer _ ->
+            Integer 0
+
+        NonNegative _ ->
+            NonNegative 0
+
+        Step by _ ->
+            Step by 0
+
+
+counterView : msg -> msg -> msg -> msg -> Counter -> Html msg
+counterView decrement increment reset remove counter =
+    case counter of
+        Integer n ->
+            div []
+                [ button [ onClick decrement ] [ text "-" ]
+                , text (String.fromInt n)
+                , button [ onClick increment ] [ text "+" ]
+                , button [ onClick reset ] [ text "Reset" ]
+                , button [ onClick remove ] [ text "Remove" ]
+                ]
+
+        NonNegative n ->
+            div []
+                [ button [ onClick decrement ] [ text "-" ]
+                , text (String.fromInt n)
+                , button [ onClick increment ] [ text "+" ]
+                , button [ onClick reset ] [ text "Reset" ]
+                , button [ onClick remove ] [ text "Remove" ]
+                ]
+
+        Step by n ->
+            div []
+                [ button [ onClick decrement ] [ text ("-" ++ String.fromInt by) ]
+                , text (String.fromInt n)
+                , button [ onClick increment ] [ text ("+" ++ String.fromInt by) ]
+                , button [ onClick reset ] [ text "Reset" ]
+                , button [ onClick remove ] [ text "Remove" ]
+                ]
 
 
 -- HELPERS
